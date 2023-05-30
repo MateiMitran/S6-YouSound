efd25b602009c2f947d4cd146c98c5976ee7c58a
Mitran Matei-Cristian <matei.mitran10@gmail.com>
2023-05-30 22:08:04 +0200

    Added sprint 4 presentation

--- backend/yousound/music-service/src/main/java/com/backend/musicservice/config/CorsConfig.java
+++ backend/yousound/music-service/src/main/java/com/backend/musicservice/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.backend.musicservice;
+package com.backend.musicservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
